37fa202396104d69bdb5522dafc2c84131e03c5f
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-27 20:57:01 -0400

    added some action code, initialized path and grid

--- assignment1/PacSimRNNA.java
+++ assignment1/PacSimRNNA.java
@@ -20,12 +20,48 @@ public class PacSimRNNA implements PacAction
     private List<Point> path;
     private int simTime;
 
+    public PacSimRNNA(String fname)
+    {
+        PacSim sim = new PacSim(fname);
+        sim.init(this);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("\nTSP using Repetitive Nearest Neighbor Algorithm by Kobee Raveendran:");
+        System.out.println("\nMaze : " + args[0] + "\n");
+
+        new PacSimRNNA(args[0]);
+    }
 
     @Override
     public void init()
     {
         simTime = 0;
         path = new ArrayList();
+        numMoves = 0;
+    }
+
+    public PacFace action(Object state)
+    {
+        PacCell[][] grid = (PacCell[][]) state;
+        PacmanCell pc = PacUtils.findPackman(grid);
+
+        if (pc == null)
+        {
+            return null;
+        }
+
+        if (path.isEmpty())
+        {
+            Point target = PacUtils.nearestFood(pc.getLoc(), grid);
+            path = BFSPath.getPath(grid, pc.getLoc(), target);
+
+            System.out.println("Pac-Mac current at: [ " + pc.getLoc().x + ", " + pc.getLoc().y + " ]");
+            System.out.println("Setting new target  : [ " + target.x + ", " + target.y + " ]");
+            
+            
+        }
     }
 
 }
