be0060551b49d46102c759923593b13df8a5ba0e
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-28 21:21:59 -0400

    added initial version of cost matrix generator

--- PacSimRNNA.java
+++ PacSimRNNA.java
@@ -42,6 +42,49 @@ public class PacSimRNNA implements PacAction
         numMoves = 0;
     }
 
+    public static int[][] createCostMatrix(PacmanCell pc, PacCell[][] grid)
+    {
+        // retrieve nodes of fully-connected graph for RNNA cost matrix (the food points)
+        List<Point> food = PacUtils.findFood(grid);
+        int foodCount = food.size();
+
+        int[][] matrix = new int[foodCount + 1][foodCount + 1];
+
+        // populate cost matrix from every node to every other node (with zeros in diagonal)
+        for(int i = 0; i <= foodCount; i++)
+        {
+            // fill in columns B - ... (from one food cell to all other food cells)
+            for(int j = 1; j < i; j++)
+            {
+                int cost = BFSPath.getPath(grid, food.get(i), food.get(j)).size();
+
+                matrix[i][j] = cost;
+                matrix[j][i] = cost;
+            }
+            
+            int costFromPac = BFSPath.getPath(grid, pc.getLoc(), food.get(i)).size();
+            
+            // fill in column A and row A (from Pacman to each food cell)
+            matrix[0][i] = costFromPac;
+            matrix[i][0] = costFromPac;          
+        }
+
+        // print cost matrix
+        System.out.println("Cost table:");
+        
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for(int j = 0; j < matrix.length; j++)
+            {
+                System.out.print("   " + matrix[i][j]);
+            }
+            
+            System.out.println();
+            
+        }
+    }
+
+    @Override
     public PacFace action(Object state)
     {
         PacCell[][] grid = (PacCell[][]) state;
@@ -52,6 +95,7 @@ public class PacSimRNNA implements PacAction
             return null;
         }
 
+        // decision to be made when deciding which food to go for next
         if (path.isEmpty())
         {
             Point target = PacUtils.nearestFood(pc.getLoc(), grid);
